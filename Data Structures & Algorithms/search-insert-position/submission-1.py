4d1d89e3cc76f9ff8b4940cce0e5c6b7d65f842c
lokesh628 <140999443+lokesh628@users.noreply.github.com>
2026-04-29 16:11:11 +0530

    Add: search-insert-position - submission-1

---
+++ Data Structures & Algorithms/search-insert-position/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l,h=0,len(nums)-1
+        while(l<=h):
+            mid=(l+h)//2
+            if nums[mid]>target:
+                h=mid-1
+            elif nums[mid]<target:
+                l=mid+1
+            else:
+                return mid
+        return l
+
